61dab4596211334a57e1a0097e03244d0a14e8d0
ashic <ashic@live.com>
2017-12-07 15:57:20 +0000

    enabled ignoring of ssl checking of client

--- start.sh
+++ start.sh
@@ -5,8 +5,13 @@ sed -i "s/{{TARGET_SERVICE}}/${TARGET_SERVICE}/g;" /etc/nginx/nginx.conf
 
 if [ "x$USE_SELF_SIGNED_CERT" != "x" ]; then
   echo "Using self signed certificates as USE_SELF_SIGNED_CERT is set."
+  export DO_NOT_VERIFY_SSL_CLIENT="1"
   mkdir -p /etc/nginx/certs
   cp /self-signed-cert/* /etc/nginx/certs/ 
+fi
+
+if [ "x$DO_NOT_VERIFY_SSL_CLIENT" != "x" ]; then
+  sed -i "/.*ssl_verify_client.*/d" /etc/nginx/nginx.conf
 fi
 
 echo "Starting nginx..."
