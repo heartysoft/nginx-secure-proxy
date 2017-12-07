59d4cc050686f65603fccc3d5e845a3330ed6ae4
ashic <ashic@live.com>
2017-12-07 14:58:07 +0000

    adding feature to use self signed cert if env var set

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ LABEL maintainer "luis@bigcente.ch"
 
 ADD config/nginx.conf /etc/nginx/nginx.conf
 ADD start.sh /usr/bin/start.sh
+ADD self-signed-cert/ /self-signed-cert
 RUN chmod +x /usr/bin/start.sh
 
 CMD /usr/bin/start.sh
--- start.sh
+++ start.sh
@@ -3,5 +3,11 @@
 # Tell nginx the address and port of the service to proxy to
 sed -i "s/{{TARGET_SERVICE}}/${TARGET_SERVICE}/g;" /etc/nginx/nginx.conf
 
+if [ "x$USE_SELF_SIGNED_CERT" != "x" ]; then
+  echo "Using self signed certificates as USE_SELF_SIGNED_CERT is set."
+  mkdir -p /etc/nginx/certs
+  cp /self-signed-cert/* /etc/nginx/certs/ 
+fi
+
 echo "Starting nginx..."
 nginx -g 'daemon off;'
